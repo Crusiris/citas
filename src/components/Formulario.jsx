e96d9b82b59101aa63af2ef927bdbbc603c1b0a0
crusiris <cruzsirys@gmail.com>
2020-04-13 17:37:18 -0400

    Generando un id unico para cada cita

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,23 +1,69 @@
 import React, { Fragment, useState } from 'react'
+//import uuid from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
 
 
-//State citas
-const [ appointment, updateAppointment ] = useState({
-    pet:'',
-    owner:'',
-    date:'',
-    hour:'',
-    symptom:''
-})
+
+
 
 const Formulario = () => {
-//Funcion que se ejecuta cada vez que el usario escribe en un input
-const handleChange = ()=>{}
+
+    //State citas
+    const [ appointment, updateAppointment ] = useState({
+            pet:'',
+            owner:'',
+            date:'',
+            hour:'',
+            symptom:''
+    })
+
+    //State de error
+    const [ error, updateError ] = useState(false)
+
+    //Funcion que se ejecuta cada vez que el usario escribe en un input
+    const handleChange = e =>{
+        updateAppointment({
+            ...appointment,
+            [e.target.name]:e.target.value
+        })
+
+    }
+
+    //Extraer valos del estado con un destructrin
+    // para evitar usar sintaxis largas como appointment.pet etc
+
+    const { pet, owner, date, hour, symptom } = appointment
+
+    //Agregar cita
+    const submitAppointment = e =>{
+    e.preventDefault();
+        //Validar
+    if( pet.trim() === '' || owner.trim() === '' || date.trim() === '' ||  hour.trim() === '' || symptom.trim() === ''){
+        //llamo la funcion que actualiza el estado 
+        updateError(true)
+        return;
+    }
+    //Eliminando el mensaje de error
+    updateError(false)
+   
+    //Generando el id, a traves de la librera uuid
+    appointment.id = uuidv4();
+    console.log(appointment);
+
+    }
 
     return (
         <Fragment>
         <h2>Crear cita</h2> 
-        <form>
+        
+            {error 
+            ? <p className="alerta-error">Todos los campos son obligatorios</p>
+            : null
+            }
+
+        <form
+        onSubmit = { submitAppointment }
+        >
 
         <label>Nombre Mascota</label>
         <input
@@ -26,6 +72,7 @@ const handleChange = ()=>{}
         className="u-full-width"
         placeholder="Nombre de mascota"
         onChange = {handleChange}
+        value = { pet }
         />
 
         <label>Nombre Dueño</label>
@@ -35,6 +82,7 @@ const handleChange = ()=>{}
         className="u-full-width"
         placeholder="Nombre del dueño de la mascota"
         onChange = {handleChange}
+        value = { owner }
         />
 
         <label>Fecha</label>
@@ -43,6 +91,7 @@ const handleChange = ()=>{}
         name="date"
         className="u-full-width"
         onChange = {handleChange}
+        value = { date }
         />
 
         <label>Hora</label>
@@ -51,6 +100,7 @@ const handleChange = ()=>{}
         name="hour"
         className="u-full-width"
         onChange = {handleChange}
+        value = { hour }
         />
 
         <label>Sintomas</label>
@@ -58,6 +108,7 @@ const handleChange = ()=>{}
         name="symptom"
         className="u-full-width"
         onChange = {handleChange}
+        value = { symptom }
         ></textarea>
 
         <button
