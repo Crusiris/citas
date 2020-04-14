b6c99b09dc6cb09f4a516f156a24ac58e332137b
crusiris <cruzsirys@gmail.com>
2020-04-14 13:18:44 -0400

    Guardando citas en el localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, { Fragment,useState } from 'react';
+import React, { Fragment,useState , useEffect } from 'react';
 import Formulario from './components/Formulario'
 import Cita from './components/Cita'
 
 
 function App() {
 
+//Citas en local storage
+let initialQuotes = JSON.parse(localStorage.getItem('appointments'));
+//Sino hay citas iniciales, entonces las citas iniciales 
+//sera igual a un arreglo vacio
+if(!initialQuotes){
+  initialQuotes= []
+}
+
 //state de nuevas citas
-const [appointments, saveAppointment]= useState([]);
+const [appointments, saveAppointment]= useState(initialQuotes);
+
+//Use Effect para realizar ciertas operaciones
+//Cuando el state cambia
+useEffect(()=>{
+  if(initialQuotes){
+    localStorage.setItem('appointments', JSON.stringify(appointments))
+  }else{
+    localStorage.setItem('appointments', JSON.stringify([]))
+  }
+}, [appointments, initialQuotes ]);
 
 //Funcion que tome las citas actuales y agregue la nueva
 const createAppointment = appointment => {
